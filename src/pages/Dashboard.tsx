eaaaba18f215f674d9d14617b2ea6bd85dc09617
samim3ini <sjm111cia@gmail.com>
2025-03-18 14:41:12 +0300

    src

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,7 +6,7 @@ import AttendanceDashboard from './Attendance';
 const Dashboard: React.FC = () => {
   const [activeTab, setActiveTab] = useState(0);
 
-  const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+  const handleTabChange = (_: React.SyntheticEvent, newValue: number) => {
     setActiveTab(newValue);
   };
 
