93731f1b5890187e8048b1c5a3cc3262adf63851
samim3ini <sjm111cia@gmail.com>
2025-03-18 14:18:12 +0300

    SC

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ function App() {
       <div className="app-container">
         <Routes>
           <Route path="/" element={<Dashboard />} />
-          {/* Remove other routes if they are no longer needed */}
         </Routes>
       </div>
     </Router>
